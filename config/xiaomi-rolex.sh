14763f3103e4b3fa41a2425c358a48fc9947db38
Barnabás Czémán <trabarni@gmail.com>
2025-01-06 22:44:21 +0100

    config/dtb: xiaomi-rolex: add panels

---
+++ config/xiaomi-rolex.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[hx8394f_boe_720p_video]="xiaomi,rolex-hx8394f-boe"
+	[ili9881c_ebbg_720p_video]="xiaomi,rolex-ili9881c-ebbg"
+	[nt35521s_ebbg_720p_video]="xiaomi,rolex-nt35521s-ebbg"
+)
